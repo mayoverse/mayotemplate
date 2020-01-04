11c038e0982dce9c7106ec1f7311d022a0435576
overdodactyl <patjohns1216@gmail.com>
2020-01-04 14:16:47 -0500

    update bootstrap code

--- data-raw/get_bootstrap.R
+++ data-raw/get_bootstrap.R
@@ -1,2 +1,2 @@
 bootstrap_css <- mayotheme::use_mayo_css("bootstrap")
-fs::file_copy(bootstrap_css, "inst/pkgdown/assets/mayotemplate.css")
+fs::file_copy(bootstrap_css, "inst/pkgdown/assets/mayotemplate.css", overwrite = TRUE)
